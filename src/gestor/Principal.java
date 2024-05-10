4aa0eadd8b2f1e868f0fc71638f6efd7999ce230
earnesto <earnesto@106-16.info2>
2024-05-10 14:38:01 +0200

    Cambios

--- src/gestor/Principal.java
+++ src/gestor/Principal.java
@@ -1,10 +1,45 @@
 package gestor;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String fecha;
+		String lugar;
+		int precio;
+		
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new FileReader("src/gestor/datosTurismo.txt"));
+			
+			String linea;
+			linea = br.readLine();
+			
+			while(linea != null) {
+				String[] viaje = linea.split("::");
+				lugar = viaje[0];
+				fecha = viaje[1];
+				precio = Integer.parseInt(viaje[2]);
+				new Viaje(lugar, fecha, precio);
+			}
+				
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		} finally {
+			try {
+				br.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
--- src/gestor/Viaje.java
+++ src/gestor/Viaje.java
@@ -1,5 +1,46 @@
 package gestor;
 
 public class Viaje {
-	new 
+	private String lugar = "";
+	private String fecha = "";
+	private int precio = 0;
+
+	public Viaje(String lugar, String fecha, int precio) {
+		if (lugar != null && !lugar.equals(fecha)) {
+			this.lugar = lugar;
+		}
+		if (fecha != null && !fecha.equals("")) {
+			this.fecha = fecha;
+		}
+		if (precio > 0) {
+			this.precio = precio;
+		}
+	}
+
+	public String getLugar() {
+		return lugar;
+	}
+
+	public void setFecha(String fecha) {
+		if (fecha != null && !fecha.equals("")) {
+			this.fecha = fecha;
+		}
+	}
+
+	public void setPrecio(int precio) {
+		if (precio > 0) {
+			this.precio = precio;
+		}
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		boolean igual = false;
+		Viaje viaje = (Viaje) obj;
+		if (viaje.lugar.equals(this.lugar) && viaje.fecha.equals(this.fecha)) {
+			igual = true;
+		}
+		return igual;
+	}
+
 }
