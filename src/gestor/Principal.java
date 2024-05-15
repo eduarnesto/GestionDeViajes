5919a56e2a3ca061136f189ed780bfa9a320f6a5
Jose Manuel <Jose Manuel@192.168.1.39>
2024-05-15 18:55:32 +0200

    introduccion de comentario de codigo

--- src/gestor/Principal.java
+++ src/gestor/Principal.java
@@ -7,32 +7,36 @@ import excepciones.ExcepcionLugar;
 import excepciones.ExcepcionPrecio;
 
 public class Principal {
-
+//creamos el scanner para poder leer por consola
 	static Scanner scanner = new Scanner(System.in);
 	
 	public static void main(String[] args) {
+		//creamos la variable donde guardaremos la opcion elegida por el usuario
 		int opcion;
+		//creamos la entrada al programa 
 		System.out.println("Hola");
 		do {
 			System.out.println("Hola");
 			ControlArchivo.leerArchivo();
 			System.out.println("Hola");
+			//llamamos al menu
 			menu();
-
+			//guardamos la opcion dada por el usuario
 			opcion = scanner.nextInt();
-
+			//limpiamos el scanner
 			scanner.nextLine();
-
+			//creamos una funcion para realizar las diferentes operaciones dependiendo de lo que elija el usuario
 			switch (opcion) {
-
+			//en el primer caso llamamos a la funcion donde tenemos los viajes
 			case 1:
 				Gestion.listar();
 				break;
-
+			//aqui llamamos a la funcion que añade viajes y le pedirá mas datos al usuario
 			case 2:
 				anyadirViaje();
 				break;
 				/*
+				 //el siguiente modifica viajes 
 			case 3:
 
 				System.out.print("Lugar del viaje a modificar: ");
@@ -45,7 +49,7 @@ public class Principal {
 
 
 				break;
-
+			//el siguiente llama a la funcion que los elimina
 			case 4:
 
 				System.out.print("Lugar del viaje a eliminar: ");
@@ -55,17 +59,18 @@ public class Principal {
 				Gestion.borrarViaje(lugar);
 
 				break;
-
+			//este llama a la funcion que almacena y actualiza todos los cambios
 			case 5:
 
 				ControlArchivo.imprimirArchivo();
 
 				break;
 */
+			//con este ultimo sales del programa
 			case 6:
 				System.out.println("Has salido del programa");
 				break;
-
+			//el default avisa de que es una opcion no valida ya sea por introducir algo que no sea un numero o un numero que no aparezca en la casuistica
 			default:
 
 				System.out.println("Opción no valida.");
@@ -73,15 +78,20 @@ public class Principal {
 			}
 
 		} while (opcion != 9);
-
+		//cerramosel scanner para terminar el main
 		scanner.close();
 	}
-
+	//esta funcion es la que nos ayuda a añadir nuevos viajes
 	public static void anyadirViaje() {
+		//variable que guarda el lugar del nuevo viaje
 		String lugar;
+		//variable que guarda la fecha del nuevo viaje
 		String fecha;
+		//variable que guarda el precio del nuevo viaje
 		int precio;
+		//variable para comprobar si ese viaje ya existe
 		Viaje viaje = null;
+		//ahora le pedimos por consola los datos al usuario
 		System.out.print("Lugar del viaje: ");
 
 		lugar = scanner.nextLine();
@@ -93,7 +103,7 @@ public class Principal {
 		System.out.print("Precio del viaje: ");
 
 		precio = scanner.nextInt();
-
+		//comprobamos si el viaje ya existe  y si se ha podido añadir 
 		try {
 			viaje = new Viaje (lugar, fecha, precio);
 		} catch (ExcepcionLugar | ExcepcionFecha | ExcepcionPrecio e) {
@@ -101,13 +111,13 @@ public class Principal {
 		}
 		
 		if (Gestion.anyadirViaje(viaje)) {
-			System.out.println("Viaje anyadido correctamente");
+			System.out.println("Viaje añadido correctamente");
 		} else {
 			System.out.println("No se ha podido añadir el viaje");
 		}
 		
 	}
-
+	//funcion para imprimir el menu por consola 
 	public static void menu() {
 		System.out.println("Bienvenido al gestor de viajes Volando Voy");
 		System.out.println("1. Ver viajes disponibles");
